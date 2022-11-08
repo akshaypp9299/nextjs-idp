97dbbf1785551927f330427d71dca5a504f0fbe2
Akshay Polshettiwar <akshay.polshettiwar@xecurify.com>
2022-11-08 15:43:38 +0530

    Third Commit with trial form implemented

--- components/TrialForm.js
+++ components/TrialForm.js
@@ -1,42 +1,76 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import {dataHandler} from "../pages/api/trialformapi";
+import { faCheck, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+import { dataHandler } from "../pages/api/trialformapi";
 
 export default function TrialForm() {
-   
-   const defaultData= {
-    
-    email:"",
-    query:"",
-    firstName:"Akshay",
-    lastName:"aksfhay",
-    phone:"akshay",
-    company:"miniO",
-    ccEmail:"samlsupport@xecurify.com",
-    look:"TRIAL REQUEST : WordPress SAML / WS-FED IDP"
 
-   }
+    const[isSuccess,setIsSuccess] = useState(false);
 
-   const [data,setData] = useState(defaultData);
+    const defaultData = {
 
-   const valueHandler = (e) =>{
-        setData(function(prevState){
+        email: "",
+        query: "",
+        firstName: "",
+        lastName: "",
+        phone: "",
+        company: "",
+        ccEmail: "samlsupport@xecurify.com",
+    }
+
+    const [data, setData] = useState(defaultData);
+
+    const valueHandler = (e) => {
+        setData(function (prevState) {
             return ({
                 ...prevState,
-                [e.target.name] : e.target.value,
+                [e.target.name]: e.target.value
             })
         })
-   }
+    }
 
-   
-   const sendData = async(event) =>{
-    event.preventDefault();
-    await dataHandler(data);
-   }
-   
+    const [pageURL, setPageURL] = useState("");
+    const [refferer, setPageReferer] = useState("");
+
+    const [queryMssg, setQueryMssg] = useState(data.query);
+
+    useEffect(() => {
+        setQueryMssg(() => `<b>[Query for TRIAL REQUEST: WORDPRESS SAML/ WS-FED IDP]: </b>  ${data.query} <br/> <b>Page_URL:</b>  ${pageURL} <br/> <b?Refferer:</b>  ${refferer}`);
+    }, [data.query])
+
+    useEffect(() => {
+        setPageURL(function (prevState) {
+            return window.location.href;
+        })
+        setPageReferer(function (prevState) {
+            return document.referrer;
+        })
+    })
+
+
+
+    const finalData = {
+        ...data, query: queryMssg
+    }
+
+
+    const sendData = async (event) => {
+        event.preventDefault();
+        await dataHandler(finalData);
+        setIsSuccess(true);
+        setTimeout(()=>{
+            setIsSuccess(false);
+        },3000);
+        
+        
+    }
+
     return (
         <>
+            {isSuccess &&         
+                <p className='mo-success'><FontAwesomeIcon icon={faCheckCircle}/> &nbsp;Thank you for the response. We will get back to you soon.</p>
+            }
+            
             <div className="container-fluid" id="request-free-trial">
                 <div className="row mt-5 section-testimonials saml-scroll" id="mo_external_contact_form">
                     <h3 className="h1 mo-saml-free-trial-head size form-title text-center">Get Full - Featured Trial</h3>
@@ -50,13 +84,13 @@ export default function TrialForm() {
                     </div>
 
                     <div className="col-md-5 mt-5 mo-saml-free-trial-form">
-                        <h3 className="h1 form-title text-center" style={{fontWeight:"400"}}>No Credit Card Required</h3>
+                        <h3 className="h1 form-title text-center" style={{ fontWeight: "400" }}>No Credit Card Required</h3>
                         <form action="" method="POST" className="mo-saml-contact-form" onSubmit={sendData} id="mo_external_contact_form">
                             <label htmlFor="free-trial-email">Email <span className="text-danger">*</span></label>
                             <input type="email" name="email" placeholder="Enter valid email" id="mo_external_contact_form_email" required="" onChange={valueHandler} />
 
                             <label htmlFor="free-trial-text">Requirement <span className="text-danger">*</span></label>
-                            <textarea name="query" placeholder="Tell us about your requirement"  id="mo_external_contact_form_query" onChange={valueHandler} required="" ></textarea >
+                            <textarea name="query" placeholder="Tell us about your requirement" id="mo_external_contact_form_query" onChange={valueHandler} required="" ></textarea >
 
                             <input hidden id="mo_external_contact_form_support" defaultValue="samlsupport@xecurify.com" />
 
